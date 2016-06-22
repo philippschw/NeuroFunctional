2a278ef481c0937e4c955a0f5c4008470f6c57eb
Hendra H2c <hendra.h2c.x@gmail.com>
2016-06-22 18:32:32 +0200

    update required files

--- examples/imagenet/make_imagenet_mean.sh
+++ examples/imagenet/make_imagenet_mean.sh
@@ -2,9 +2,9 @@
 # Compute the mean image from the imagenet training lmdb
 # N.B. this is available in data/ilsvrc12
 
-EXAMPLE=examples/imagenet
-DATA=data/check
-TOOLS=build/tools
+EXAMPLE=../../examples/imagenet
+DATA=../../data/check
+TOOLS=../../build/tools
 
 $TOOLS/compute_image_mean $EXAMPLE/ava_train_lmdb \
   $DATA/imagenet_mean.binaryproto
